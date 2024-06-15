51d0990310f9d4d27bea1a21f697f0984b7a4911
brian <brianjohnkiama@gmail.com>
2024-06-15 19:05:33 +0300

    rendering lists in ListGroup.tsx

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,30 @@
-// import { Fragment } from "react";
-// Fragment
+
 function ListGroup() {
-  // typescript uses className not just class
+    // now we work on how to render dynamic data list
+
+    const items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+    // in javascript arrays have a method called map for mapping or converting each item to an item of a different type
+    // here we want to convert each item to an item of an li element
+    // in JSX we use curly braces to render data dynamically and here we are rendering the item itself
+    items.map(item => <li>{item}</li>)
   return (
-    // NB. A component cannot return more than 1 element thus need to wrap whole with a tag
-    // it's best to use fragment.This helps ensure when this is rendered in 
+   
     <>
         <h1>List</h1>
+        {/* in jsx we don't have a for loop thus cant use it here */}
         <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
+          {/* in JSX we can only use html or any other react components thus we need to change for the below expression*/}
+          {/* items.map(item => <li>{item}</li>) */}
+          {/* to render the above expression dynamically we need to wrap it in braces */}
+          {/* each item above should have a unique key to identify it. This helps react know what part of the page should be updated  when an item is added or deleted*/}
+          {/* in this case each item is a unique string thus we can use the item itself as a unique key but in a real world application where we retrieve items from an api quite often each item has a property like id eg item.id , we dont have it in this case so we don't have o worry about it*/}
+          { items.map(item => <li key={item}>{item}</li>)}
         </ul>
     </>
   );
