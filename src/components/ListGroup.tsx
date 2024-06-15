7423e7464fdf9ac7b74658a95e4a787bd9268671
brian <brianjohnkiama@gmail.com>
2024-06-15 19:49:32 +0300

    using an if statement

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,31 +1,30 @@
-
 function ListGroup() {
-    // now we work on how to render dynamic data list
+  // we have changed const to let to enable us reset the list of items below to an empty array
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
 
-    const items = [
-        'New York',
-        'San Francisco',
-        'Tokyo',
-        'London',
-        'Paris'
-    ];
-    // in javascript arrays have a method called map for mapping or converting each item to an item of a different type
-    // here we want to convert each item to an item of an li element
-    // in JSX we use curly braces to render data dynamically and here we are rendering the item itself
-    items.map(item => <li>{item}</li>)
+  // sometimes we wanna render different content based on certain conditions
+  // if (items.length === 0)
+  //   return (
+  //     <>
+  //       {" "}
+  //       <h1>list</h1>
+  //       <p>No item found</p>{" "}
+  //     </>
+  //   );
+
+  // in the above code, it works but there is a lot of duplication thus we are going to render things conditionally inside our jsx expression
   return (
-   
     <>
-        <h1>List</h1>
-        {/* in jsx we don't have a for loop thus cant use it here */}
-        <ul className="list-group">
-          {/* in JSX we can only use html or any other react components thus we need to change for the below expression*/}
-          {/* items.map(item => <li>{item}</li>) */}
-          {/* to render the above expression dynamically we need to wrap it in braces */}
-          {/* each item above should have a unique key to identify it. This helps react know what part of the page should be updated  when an item is added or deleted*/}
-          {/* in this case each item is a unique string thus we can use the item itself as a unique key but in a real world application where we retrieve items from an api quite often each item has a property like id eg item.id , we dont have it in this case so we don't have o worry about it*/}
-          { items.map(item => <li key={item}>{item}</li>)}
-        </ul>
+      <h1>List</h1>
+      {/* inside here we cannot write an if statement because as stated earlier , here we can only use html elements or other react components but the only exception is braces, with braces we can render anything dynamically*/}
+      {items.length === 0 ? <p>No item found</p>: null }
+
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </>
   );
 }
