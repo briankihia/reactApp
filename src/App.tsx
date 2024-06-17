baa88139297d1f71b9c79238b043c251cb907b2b
brian <brianjohnkiama@gmail.com>
2024-06-17 16:25:55 +0300

    passing data via props

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@
 import ListGroup from "./components/ListGroup";
 
 function App(){
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   // we use message component in our app component.We add it just ike a normal tag
   // <Message></Message> or <Message/> are the same
-  return <div><ListGroup/></div>
+  // in cities below we did not use braces because we are passing static data
+  return <div><ListGroup items= {items} heading="Cities"/></div>
 }
 
 // just like the message component we should export the app component
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,44 +1,42 @@
+
+interface props{
+  // above can also be written as ListGroupProps ie. fixing it with the name of the component
+  // using braces we define various properties and their types
+  // we use type annotation to specify type of various properties
+  items: string[];
+  heading: string;
+}
+
 import { useState } from "react";
 
-function ListGroup() {
+// below where we have items, heading we had prop but we use it this way to avoid repetitiveness by adding prop. before items and heading word and can be used anywhere in that fun
+function ListGroup({items, heading}: props) {
   // we have changed const to let to enable us reset the list of items below to an empty array
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // instead of defining the items above there, we should be able to pass them as an input to this component
+  // the same way we can call a function and give it an argument.The same applies to the heading of list
 
-
-  // the below function is called a hook(a function that allows us to tap into built in features in react)
-  // using useState we can tell react that this component can have data or state that will change overtime
-  // useState(-1) return an array 
-  // in this array we will have 2 elements
-  // 1. a variable like our selectedIndex variable
-  // 2. updater function(using the updater function we can update the first variable and by this react will be notified and it knows the state of our component is changing thus it will re-render our component)
-  // const arr = useState(-1);
-  // arr[0]
-  // arr[1]
-  // rather than doing above we do like below
-  // selectedIndex is the stateVariable and setSElectedIndex is a function
-  // -1 below is just a default value
+ 
   const [selectedIndex, setSelectedIndex] = useState(-1);
-  // we use state hook to declare another variable called name. that is the idea
-  // const [name, setName]= useState('');
-  // one thing to know about component state is that each component is going to have it's own state thus if you go back to app component and add another instance of ListGroup, each list group is going to have it's own state
+ 
   return (
     <>
-      <h1>List</h1>
+    {/* instead of hard coding the below label there, it would be nicer if you could pass different labels like cities , names etc */}
+    {/* to do that first we need to decide the shape of the input to this component */}
+    {/* thus we should be able to pass an object with 2 properties*/}
+    {/* 1. items which is going to be an array and heading which is going to be a string . To do this we use one of typescript feature called an interface */}
+    {/* using an interface we can define the shape or the interface of an object */}
+    {/* below we add the heading dynamically */}
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         
         {items.map((item, index) => (
          
           <li
-          // when we click an item we want to highlight it thus we add a bootstrap class that does that called active
-          // but we dont want to highlight all at once thus to do that we need a variable to keep track of the index of the selected item
-          // below we want to render the active class dynamically so
-            // className="list-group-item active"
-            // here we have done such that if our selectedIndex is the current one we apply the following classes otherwise if not we just give it the list-group-item
+          
             className= {selectedIndex === index? 'list-group-item active' : 'list-group-item'}
             key={item}
-           
-            // in below we have written a simple error function which sets selected index to the index of the current item
             onClick = {()=>{setSelectedIndex(index);}}
           >
             {item}
